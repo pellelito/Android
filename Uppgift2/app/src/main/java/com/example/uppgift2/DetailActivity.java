a100e0e9643efb081504266dc79575a9318778e8
pellelito <pelle.lofstrand@gmail.com>
2020-04-13 12:25:10 +0200

    Final layout

--- Uppgift2/app/src/main/java/com/example/uppgift2/DetailActivity.java
+++ Uppgift2/app/src/main/java/com/example/uppgift2/DetailActivity.java
@@ -2,8 +2,18 @@ package com.example.uppgift2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.Display;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -12,9 +22,58 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        Intent in = new getIntent();
-        int index = in.getIntExtra("ITEM_INDEX");
+        Intent in = getIntent();
+        int index = in.getIntExtra("ITEM_INDEX", -1);
+        if (index > -1) {
+            int pic = getImg(index);
+            ImageView img = (ImageView) findViewById(R.id.imageView);
+            scaleImg(img, pic);
+        }
 
+        Button myBtn = (Button)findViewById(R.id.myBtn);
+        myBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                // Let's add a vibrator, why not?
+                Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    vibrator.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    //deprecated in API 26
+                    vibrator.vibrate(500);
+                }
+            }
+        });
+    }
+
+    private int  getImg(int index){
+        switch (index){
+            case 0: return R.drawable.peach;
+            case 1: return R.drawable.tomato;
+            case 2: return R.drawable.squash;
+            default: return -1;
+        }
+    }
+
+    private void scaleImg(ImageView img, int pic){
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imgWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if (imgWidth > screenWidth){
+            int ratio = Math.round( (float)imgWidth / (float)screenWidth);
+            options.inSampleSize = ratio;
+
+        }
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImg = BitmapFactory.decodeResource(getResources(), pic, options);
+        img.setImageBitmap(scaledImg);
     }
 }
