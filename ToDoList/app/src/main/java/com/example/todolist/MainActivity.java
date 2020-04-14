45ad12e2d438781de1623ae03065b702ac715b6f
pellelito <pelle.lofstrand@gmail.com>
2020-04-14 08:21:34 +0200

    ToDo layout upload

---
+++ ToDoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //assign button
+        Button addBtn = (Button)findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent submitTask = new Intent(getApplicationContext(),createTask.class);
+                startActivity(submitTask);
+            }
+        });
+    }
+}
